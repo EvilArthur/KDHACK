2a9546b1d97bec361dbdd2c184a392e8c2c6ac04
NikiFilik <91692887+NikiFilik@users.noreply.github.com>
2021-10-01 17:15:36 +0200

    Update geoloc.py

--- func/geoloc.py
+++ func/geoloc.py
@@ -2,4 +2,4 @@ from geopy import distance
 def distance_calc(myshirota, mydolgota, clubshirota, clubdolgota):
     mylocation = (myshirota, mydolgota)
     clublocation = (clubshirota, clubdolgota)
-    print(distance.distance(mylocation, clublocation).km)
+    return(distance.distance(mylocation, clublocation).km)
