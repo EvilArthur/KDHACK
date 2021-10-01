4779c714171dca539a7f06ec80f1a17cd95fd6b7
EvilArthur <thebestmail20@gmail.com>
2021-10-01 21:44:01 +0200

    Превратил в функцию

--- mail.py
+++ mail.py
@@ -4,24 +4,29 @@ from email.mime.multipart import MIMEMultipart
 from config import from_email, password
 from platform import python_version
 
-msg = MIMEMultipart('alternative')
 
-to_email = 'l0tus0rb@yandex.ru'
-message = 'Сообщение сделано при помощи python'
-msg['To'] = 'l0tus0rb@yandex.ru'
-msg['Subject'] = 'Тема письма'
-msg['From'] = 'GosUslugiKids <KDhack2021@yandex.ru>'
-msg['Reply-To'] = from_email
-msg['Return-Path'] = from_email
-msg['X-Mailer'] = 'Python/' + (python_version())
+def mail_out(to_email):
+    msg = MIMEMultipart('alternative')
 
-
-html = '<html><head></head><body><p>' + message + '</p></body></html>'
-msg.attach(MIMEText(message, 'plain'))
-msg.attach(MIMEText(html, 'html'))
+    message = 'Сообщение сделано при помощи python'
+    msg['To'] = to_email
+    msg['Subject'] = 'Тема письма'
+    msg['From'] = 'GosUslugiKids <KDhack2021@yandex.ru>'
+    msg['Reply-To'] = from_email
+    msg['Return-Path'] = from_email
+    msg['X-Mailer'] = 'Python/' + (python_version())
 
 
-server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
-server.login(from_email, password)
-server.sendmail(from_email, to_email, msg.as_string())
-server.quit()
+    html = '<html><head></head><body><p>' + message + '</p></body></html>'
+    msg.attach(MIMEText(message, 'plain'))
+    msg.attach(MIMEText(html, 'html'))
+
+
+    server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
+    server.login(from_email, password)
+    server.sendmail(from_email, to_email, msg.as_string())
+    server.quit()
+
+
+if __name__ == "__main__":
+    mail_out('l0tus0rb@yandex.ru')
