9b006d432c2cd3869590dfaf99308afceda8601e
EvilArthur <thebestmail20@gmail.com>
2021-10-01 19:45:06 +0200

    Добавлена тема письма, имя и почта

--- mail.py
+++ mail.py
@@ -2,13 +2,24 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from config import from_email, password
+from platform import python_version
 
-msg = MIMEMultipart()
+msg = MIMEMultipart('alternative')
 
 to_email = 'l0tus0rb@yandex.ru'
 message = 'Сообщение сделано при помощи python'
+msg['To'] = 'l0tus0rb@yandex.ru'
+msg['Subject'] = 'Тема письма'
+msg['From'] = 'GosUslugiKids <KDhack2021@yandex.ru>'
+msg['Reply-To'] = from_email
+msg['Return-Path'] = from_email
+msg['X-Mailer'] = 'Python/' + (python_version())
 
+
+html = '<html><head></head><body><p>' + message + '</p></body></html>'
 msg.attach(MIMEText(message, 'plain'))
+msg.attach(MIMEText(html, 'html'))
+
 
 server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
 server.login(from_email, password)
