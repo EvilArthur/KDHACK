946f29d8bc205f882d8cd23b6ea92d3ecda82683
lrrrtm <74620760+lrrrtm@users.noreply.github.com>
2021-10-02 03:26:22 +0200

    Add files via upload

--- func/edit_profile.py
+++ func/edit_profile.py
@@ -1,4 +1,4 @@
-@bot.message_handler(commands=['edit_profile'])
+@bot.message_handler(commands=['editprofile'])
 def showProfile(message):
     tID = message.chat.id
     bot.send_message(tID, "Данные из профиля:")
@@ -121,12 +121,15 @@ def commit_parent_name(message):
 def commit_parent_email (message):
     tID = message.chat.id
     data = message.text
-    with connection.cursor() as cursor:
-            cursor.execute("update users set parent_email = \"" +
-                           data + "\" where tID = \"" + str(tID) + "\"")
-            connection.commit()
-    bot.send_message(tID, "Электронная почта твоего родителя обновлена")
-    # ПРОВЕРКА ПОЧТЫ
+    if not validate_email(data, check_mx=True):
+        msg=bot.send_message(tID, incorrect_input_text)
+        bot.register_next_step_handler(msg, commit_parent_email)
+    else:
+        with connection.cursor() as cursor:
+                cursor.execute("update users set parent_email = \"" +
+                            data + "\" where tID = \"" + str(tID) + "\"")
+                connection.commit()
+        bot.send_message(tID, "Электронная почта твоего родителя обновлена")
 
 def commit_categories(message):
     tID = message.chat.id
